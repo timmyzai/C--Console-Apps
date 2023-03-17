ac5745dea72dfef119062c1650d26b3dc3a6ca18
timmy <>
2023-03-17 11:47:34 +0800

    git push program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using AnimalShelterManagerNamespace;
+using EncodeDecode;
+using ListGroupingNamespace;
+using ReptileTypeAnalyzerNamespace;
+using TestNamespace;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        #region EncodeDecode
+        // new AesOperation().Main();
+        #endregion
+        #region ReptileTypeAnalyzer
+        // new ReptileTypeAnalyzer().AnalyzeReptileTypes(new List<int> { 1, 13,2, 5,10, 11, 30, 39, 40,9});
+        #endregion
+        #region ListFilter
+        // new ListFilter().GetValuesGreaterThan100(new List<int>() { 10, 101, 210, 3, 310, 10010 });
+        // new ListFilter().GetAllPossiblePairs(new List<int>() { 1, 5, 10, 60, 30, 16, 38, 101 }, 100);
+        #endregion
+        #region AnimalShelterManager
+        // new AnimalShelterManager().Main();
+        #endregion
+        #region Test (Git Ignored)
+        // new Test().Main("asd","asddfasdf",3);
+        #endregion
+    }
+}
